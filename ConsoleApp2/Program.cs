899c96f32d23e2d84fe7af31ec80ee747ff969cc
Guilherme Reis <guilhermereisdl@gmail.com>
2024-01-22 13:24:06 -0300

    V1

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+internal class Program
+{
+    delegate void StringDelegate(string str);
+
+    static void Main()
+    {
+        Console.WriteLine("Digite seu nome (digite 'N' para encerrar): ");
+
+        List<string> list = new List<string>();
+
+        while (true)
+        {
+            Console.Write("Nome: ");
+            string name = Console.ReadLine();
+
+            if (name.ToLower() == "n")
+            {
+                break;
+            }
+
+            list.Add(name);
+        }
+
+        StringDelegate printNames = delegate(string x) { Console.WriteLine(x + " é um nome fei"); };
+
+
+        foreach (var name in list)
+        {
+            printNames(name);
+        }
+    }
+}
